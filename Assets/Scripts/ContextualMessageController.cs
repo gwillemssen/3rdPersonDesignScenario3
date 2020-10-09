c197304f0e2b11fb6dad397704296ef15be51ac1
gwillemssen <57201850+gwillemssen@users.noreply.github.com>
2020-10-08 22:30:08 -0500

    Set up basic coroutine for contextual message

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -14,10 +14,15 @@ public class ContextualMessageController : MonoBehaviour
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine(ShowMessage("TESTING", 2));
     }
 
-    private void ShowMessage(string message, float duration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
+        canvasGroup.alpha = 1;
         messageText.text = message;
+        yield return new WaitForSeconds(duration);
+        canvasGroup.alpha = 0;
     }
 }
